ff44b107ecb1e26a418edb17cb7b998b7699d339
Francois Lallart <fraff@free.fr>
2019-12-07 20:16:09 +0100

    update license

--- lib/ansible/modules/cloud/ovh/ovh_monthly_billing.py
+++ lib/ansible/modules/cloud/ovh/ovh_monthly_billing.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# ovh_monthly_billing, an Ansible module for managing OVH monthly billing
-# Copyright (C) 2019, Francois Lallart (@fraff)
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+# Copyright: Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 
